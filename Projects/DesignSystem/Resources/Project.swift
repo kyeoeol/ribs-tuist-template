cd9701c7bc6afee3135e5f0063013b3960a15096
kyeoeol <kyeoeol@icloud.com>
2025-09-07 15:17:57 +0900

    chore(designsystem): remove obsolete Resources module and assets placeholder

--- Projects/DesignSystem/Resources/Project.swift
+++
@@ -1,9 +0,0 @@
-import ProjectDescription
-import ProjectDescriptionHelpers
-
-let project = Project.makeModule(
-    layer: .designSystem,
-    name: "Resources",
-    product: .framework,
-    requiresResources: true
-)
